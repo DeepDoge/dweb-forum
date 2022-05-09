ad4a6632700b3ceaddd1beffb1223ea6f896680e
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-05-09 13:17:52 +0000

    ...

--- src/plugins/hardhat/contracts/App.sol
+++ src/plugins/hardhat/contracts/App.sol
@@ -1,48 +1,96 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity 0.8.13;
 
 contract App {
+    /* 
+    ==========================
+    Nickname
+    ==========================
+    */
     mapping(address => string) public walletToNicknameMap;
     event NicknameClaimed(address indexed owner, string nickname);
 
     function claimNickname(string memory nickname) public {
-        require(
-            bytes(nickname).length <= 64,
-            "Length can't be longer than 64 ASCII characters."
-        );
+        require(bytes(nickname).length <= 64, "Length can't be longer than 64 ASCII characters.");
         walletToNicknameMap[msg.sender] = nickname;
         emit NicknameClaimed(msg.sender, nickname);
     }
 
+    /* 
+    ==========================
+    Post
+    ==========================
+    */
+    Post[] public posts;
     struct Post {
         uint256 id;
-        string content; // can be a markdown or anything else, parsed by client
+        string content; // can be a markdown, ipfs hash or anything else, parsed by client
         address owner;
         uint256 publishTime;
     }
-    Post[] public posts;
 
     function postsLength() public view returns (uint256) {
         return posts.length;
     }
 
-    mapping(address => uint256[]) public ownerTimeline;
+    mapping(TimelineIdType => mapping(uint256 => TimelinePost[])) timelines;
+    enum TimelineIdType
+    {
+        Normal,
+        Reply
+    }
+    struct TimelineId
+    {
+        TimelineIdType idType;
+        uint256 id;
+    }
+    function getTimeline(TimelineId memory timelineId) private view returns (TimelinePost[] storage)
+    {
+        return timelines[timelineId.idType][timelineId.id];
+    }
+    struct TimelinePost {
+        uint256 postIndex;
+        uint256 beforePostIndex;
+        uint256 afterPostIndex;
+    }
 
-    function ownerTimelineLength(address owner) public view returns (uint256) {
-        return ownerTimeline[owner].length;
+    function timelineLength(TimelineId memory timelineId) public view returns (uint256) {
+        return getTimeline(timelineId).length;
     }
 
     uint256 public constant PUBLISH_GAS = 100;
 
-    function publishPost(string memory content) public payable {
+    function publishPost(TimelineId memory timelineId, string memory content) public payable {
         uint256 cost = tx.gasprice * PUBLISH_GAS;
         require(msg.value >= cost, "Not enough fee paid to post.");
         payable(msg.sender).transfer(msg.value - cost);
-        ownerTimeline[msg.sender].push() = posts.length;
-        posts.push() = Post(posts.length, content, msg.sender, block.timestamp);
+
+        uint256 index = posts.length;
+        posts.push() = Post(index, content, msg.sender, block.timestamp);
+        addPostToTimeline(timelineId, index);
     }
 
-    mapping(uint256 => mapping(string => uint256)) public postTagScore;
+    function addPostToTimeline(TimelineId memory timelineId, uint256 postIndex) private {
+        TimelinePost[] storage timeline = getTimeline(timelineId);
+        uint256 index = timeline.length;
+        timeline.push() = TimelinePost(postIndex, index - 1, 0);
+        timeline[index - 1].afterPostIndex = index;
+    }
+
+    function hidePostOnTimeline(TimelineId memory timelineId, uint256 postOnTimelineIndex) public {
+        TimelinePost[] storage timeline = getTimeline(timelineId);
+        require(posts[timeline[postOnTimelineIndex].postIndex].owner == msg.sender, "You are not the owner.");
+        if (postOnTimelineIndex > 0) timeline[postOnTimelineIndex - 1].afterPostIndex = timeline[postOnTimelineIndex].afterPostIndex;
+        if (postOnTimelineIndex < timeline.length - 1)
+            timeline[postOnTimelineIndex + 1].beforePostIndex = timeline[postOnTimelineIndex].beforePostIndex;
+    }
+
+    /* 
+    ==========================
+    Tag
+    ==========================
+    */
+/*     mapping(uint256 => mapping(string => uint256)) public postTagScore;
 
     mapping(string => uint256[]) public tagTimeline;
     mapping(string => mapping(uint256 => bool)) public tagTimelineIncludes;
@@ -67,10 +115,9 @@ contract App {
         if (calculateForTopTags)
             for (uint256 rank = 0; rank < TOP_TAG_COUNT; rank++)
                 if (score > topTagsOfPost[postId][rank].score) {
-                    for (uint256 x = TOP_TAG_COUNT - 1; x > rank; x--)
-                        topTagsOfPost[postId][x] = topTagsOfPost[postId][x - 1];
+                    for (uint256 x = TOP_TAG_COUNT - 1; x > rank; x--) topTagsOfPost[postId][x] = topTagsOfPost[postId][x - 1];
                     topTagsOfPost[postId][rank] = TopTag(tag, score);
                     break;
                 }
-    }
+    } */
 }
