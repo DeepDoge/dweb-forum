b234423ff9c5032eafe489e3e27c999bd2704285
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-05-31 21:13:50 +0000

    yeah now i need to rewrite the front end

--- src/plugins/hardhat/contracts/App.sol
+++ src/plugins/hardhat/contracts/App.sol
@@ -8,11 +8,11 @@ contract App {
     ==========================
     */
     mapping(address => mapping(uint256 => string)) public profiles;
-    event ProfileSet(address indexed owner, uint256 indexed key, string value);
+    event ProfileSet(address indexed owner, uint256 indexed key, string value, uint256 timestamp);
 
     function setProfile(uint256 key, string memory value) public {
         profiles[msg.sender][key] = value;
-        emit ProfileSet(msg.sender, key, value);
+        emit ProfileSet(msg.sender, key, value, block.timestamp);
     }
 
     /* 
@@ -32,15 +32,9 @@ contract App {
         uint256 id;
     }
     struct Timeline {
-        PostLink[] postLinks;
-        uint256 startIndex;
-        uint256 endIndex;
+        uint256[] postIndexes;
     }
-    struct PostLink {
-        uint256 postIndex;
-        uint256 beforePostIndex;
-        uint256 afterPostIndex;
-    }
+
 
     function _getTimeline(TimelineId memory timelineId) private view returns (Timeline storage) {
         return timelines[timelineId.group][timelineId.id];
@@ -50,25 +44,39 @@ contract App {
         return timelines[timelineId.group][timelineId.id];
     }
 
-    function getTimelinePost(TimelineId memory timelineId, uint256 timelinePostIndex) public view returns (PostLink memory) {
+    struct PostData
+    {
+        Post post;
+        ContentData content;
+    }
+    function getTimelinePost(TimelineId memory timelineId, uint256 timelineIndex) public view returns (PostData memory) {
         Timeline storage timeline = _getTimeline(timelineId);
-        return timeline.postLinks[timelinePostIndex];
+        Post memory post = posts[timeline.postIndexes[timelineIndex]];
+        ContentData[] storage history = postContentHistory[post.index];
+        return PostData(post, history[history.length - 1]);
     }
 
     function timelineLength(TimelineId memory timelineId) public view returns (uint256) {
-        return _getTimeline(timelineId).postLinks.length;
+        return _getTimeline(timelineId).postIndexes.length;
     }
 
     Post[] public posts;
     struct Post {
         uint256 index;
-        string content; // can be a markdown, ipfs hash or anything else, parsed by client
         address owner;
-        uint256 publishTime;
     }
-
     function postsLength() public view returns (uint256) {
         return posts.length;
+    }
+
+    mapping(uint256 => ContentData[]) postContentHistory;
+    struct ContentData {
+        string content; // can be a markdown, ipfs hash or anything else, parsed by client
+        uint256 publishTime; 
+    }
+    function postContentHistoryLength(uint256 postIndex) public view returns (uint256)
+    {
+        return postContentHistory[postIndex].length;
     }
 
     uint256 public constant PUBLISH_GAS = 100;
@@ -79,30 +87,40 @@ contract App {
         payable(msg.sender).transfer(msg.value - cost);
 
         uint256 index = posts.length;
-        posts.push() = Post(index, content, msg.sender, block.timestamp);
+        posts.push() = Post(index, msg.sender);
+        editPost(index, content);
 
         addPostToTimeline(timelineId, index);
         addPostToTimeline(TimelineId(TimelineGroup.Profile, uint256(uint160(address(msg.sender)))), index);
     }
 
-    function addPostToTimeline(TimelineId memory timelineId, uint256 postIndex) private {
-        Timeline storage timeline = _getTimeline(timelineId);
-        uint256 index = timeline.postLinks.length;
-        timeline.postLinks.push() = PostLink(postIndex, timeline.endIndex, 0);
-        timeline.postLinks[timeline.endIndex].afterPostIndex = index;
-        timeline.endIndex = index;
+    modifier onlyPostOwner(uint256 postIndex)
+    {
+        Post storage post = posts[postIndex];
+        require(post.owner == msg.sender, "You don't own this post.");
+        _;
     }
 
-    function hidePostOnTimeline(TimelineId memory timelineId, uint256 postOnTimelineIndex) public {
+    event PostEdit(uint256 indexed postIndex, ContentData content, uint256 timestamp);
+    function editPost(uint256 postIndex, string memory content) public onlyPostOwner(postIndex)
+    {
+        ContentData memory contentData = ContentData(content, block.timestamp);
+        postContentHistory[postIndex].push() = contentData;
+        emit PostEdit(postIndex, contentData, block.timestamp);
+    }
+
+    mapping(uint256 => mapping(uint256 => string)) postMetadatas;
+    event PostMetadataSet(uint256 indexed postIndex, uint256 indexed key, string value, uint256 timestamp);
+    function setPostMetadata(uint256 postIndex, uint256 key, string memory value) public onlyPostOwner(postIndex)
+    {
+        postMetadatas[postIndex][key] = value;
+        emit PostMetadataSet(postIndex, key, value, block.timestamp);
+    }
+
+    event TimelineAddPost(TimelineId indexed timelineId, uint256 indexed postIndex, uint256 timestamp);
+    function addPostToTimeline(TimelineId memory timelineId, uint256 postIndex) private {
         Timeline storage timeline = _getTimeline(timelineId);
-        PostLink memory link = timeline.postLinks[postOnTimelineIndex];
-        if (timeline.startIndex == postOnTimelineIndex) {
-            timeline.startIndex = link.afterPostIndex;
-        } else if (timeline.endIndex == postOnTimelineIndex) {
-            timeline.endIndex = link.beforePostIndex;
-        } else {
-            timeline.postLinks[link.beforePostIndex].afterPostIndex = link.afterPostIndex;
-            timeline.postLinks[link.afterPostIndex].beforePostIndex = link.beforePostIndex;
-        }
+        timeline.postIndexes.push() = postIndex;
+        emit TimelineAddPost(timelineId, postIndex, block.timestamp);
     }
 }
